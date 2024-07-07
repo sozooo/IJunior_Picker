d9145bccba66982d0bd450831b30a311cb2ee70c
sozooo <110661290+sozooo@users.noreply.github.com>
2024-07-07 16:03:24 +0300

    fix

--- Assets/Scripts/Picker/PickerAnimator.cs
+++ Assets/Scripts/Picker/PickerAnimator.cs
@@ -6,11 +6,8 @@ using UnityEngine;
 public class PickerAnimator : MonoBehaviour
 {
     private const string WalkParameter = "Walk";
-    private const string GatherParameter = "Gather";
 
     private Animator _animator;
-
-    private bool _isGathering;
 
     private void Awake()
     {
@@ -20,26 +17,5 @@ public class PickerAnimator : MonoBehaviour
     public void Walk()
     {
         _animator.SetBool(WalkParameter, true);
-    }
-
-    private void EndGather()
-    {
-        _isGathering = false;
-        _animator.SetBool(GatherParameter, _isGathering);
-
-        Walk();
-    }
-
-    public IEnumerator Gather()
-    {
-        _animator.SetBool(WalkParameter, false);
-        _isGathering = true;
-
-        WaitForSeconds seconds = new WaitForSeconds(5);
-        _animator.SetBool(GatherParameter, _isGathering);
-
-        yield return seconds;
-
-        EndGather();
     }
 }
